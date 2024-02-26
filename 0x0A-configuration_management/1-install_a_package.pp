833b3e945b3ba631cb18fef8ec4841e2bc9fe0bd
Anas Asimi <anas2020cr7@gmail.com>
2024-02-26 23:35:43 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,7 +4,7 @@ package { 'flask' :
     provider => 'pip3',
     }
 # install Werkzeug from pip3
-package { 'Werkzeug' :
-    ensure   => '2.2.0',
-    provider => 'pip3',
-    }
+# package { 'Werkzeug' :
+#     ensure   => '2.2.0',
+#     provider => 'pip3',
+#     }
