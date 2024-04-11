fbddff11002c09a67b729e07b8ca71ebbcffb241
Anas Asimi <anas2020cr7@gmail.com>
2024-04-11 15:41:50 +0000

    fix task 0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,5 +15,8 @@ def number_of_subscribers(subreddit: str):
         'https://www.reddit.com/r/{}/about.json'.format(subreddit),
         headers={"User-Agent": "Custom"})
     if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+        subscribers = req.json().get("data").get("subscribers")
+        if subscribers is None:
+            return 0 
+        return subscribers
     return 0
